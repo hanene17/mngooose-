ba873c3b752f2b64ac36010e23355eec89c51e92
amyLee <ghribihanen17@gmail.com>
2021-08-03 06:28:25 +0100

    5

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
-const mongoDB = require("mongodb");
-mongoose.connect(process.env.MONGO_URI);
+
+
 const { Schema } = mongoose;
 
 const personSchema = new Schema({
